2686eb8e97420e2e771930111d08a786ddcaa9cb
ts-miller <tylerstephen11@gmail.com>
2020-11-22 13:15:57 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskForm = document.getElementById("create-task-form")
+  const taskList = document.querySelector("#tasks")
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const newTask = document.querySelector("#new-task-description").value
+    taskList.innerHTML += `<li>${newTask}</li>`
+    taskForm.reset()
+  })
+})
